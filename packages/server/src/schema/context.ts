96e4205a06779c6ed31197911a4ddde5c8882397
Charles Lowell <cowboyd@frontside.io>
2021-03-18 12:38:27 +0200

    Add a person simulator
    We need to do more than just bind services. We have to be able to
    declare scenarios that arrange the state of the simulation into a
    specific order.
    
    This addse the concept of a "scenario" to the simulation and the
    given() GraphQL API that can be used to add it to the current
    simulation.
    
    As the first implementer of this scenario API is the "person"
    simulator which currently exports a single scenario called "person"
    which generates the person and inserts it into the store.
    
    The person generation logic is deliberately hokey and we can upgrade
    it to use faker / fast-check, or some combination later on.

--- packages/server/src/schema/context.ts
+++ packages/server/src/schema/context.ts
@@ -1,6 +1,8 @@
 import { Slice } from '@effection/atom';
+import assert from 'assert-ts';
 import { Task } from 'effection';
-import { SimulationState } from '../interfaces';
+import { v4 } from 'uuid';
+import { SimulationState, ScenarioState } from '../interfaces';
 
 export class SimulationContext {
   constructor(private scope: Task, private simulations: Slice<Record<string, SimulationState>>, private newid: () => string) {}
@@ -11,10 +13,24 @@ export class SimulationContext {
 
     let id = newid();
     let simulation = simulations.slice(id);
-    simulation.set({ id, status: 'new', simulators });
+    simulation.set({ id, status: 'new', simulators, scenarios: {}, store: {} });
 
-    return scope.spawn(
-      simulation.once(({ status }) => status === 'running' || status === 'failed')
-    );
+    return scope.spawn(simulation.once(settled));
   }
+
+  async given(simulationId: string, scenarioName: string): Promise<ScenarioState> {
+    let { scope, simulations } = this;
+    let simulation = simulations.slice(simulationId);
+    assert(simulation.get() != null, `no simulation found with id: ${simulationId}`);
+
+    let id = v4();
+    let scenario = simulation.slice('scenarios').slice(id);
+    scenario.set({ id, status: 'new', name: scenarioName });
+
+    return scope.spawn(scenario.once(settled));
+  }
+}
+
+function settled<T extends { status: 'new' | 'running' | 'failed' }>(value: T): boolean {
+  return value.status !== 'new';
 }
---
+++ packages/server/test/person.test.ts
@@ -0,0 +1,54 @@
+import { describe, it, beforeEach } from '@effection/mocha';
+import expect from 'expect';
+import { createSimulationServer } from '../src/server';
+import person from '../src/simulators/person';
+import { GraphQLClient, gql } from 'graphql-request';
+
+describe('person simulator', () => {
+  let client: GraphQLClient;
+
+  beforeEach(function * (world) {
+    let { port } = yield createSimulationServer({
+      simulators: { person }
+    }).run(world).address();
+
+    let endpoint = `http://localhost:${port}/graphql`;
+    client = new GraphQLClient(endpoint, { headers: {} });
+  });
+
+  describe('createSimulation()', () => {
+    let simulation: Record<string, any>;
+
+    beforeEach(function*() {
+      let createSimulationMutation = gql`
+mutation CreateSimulation {
+  createSimulation(
+    seed: 1
+    simulators: ["person"]
+  ) {
+    id
+  }
+}
+`;
+      let result = yield client.request(createSimulationMutation);
+      simulation = result.createSimulation;
+      expect(simulation.id).toBeDefined();
+    });
+
+    describe('positing a person', () => {
+      let person: Record<string, any>
+        beforeEach(function*() {
+          let { given } = yield client.request(gql`
+mutation {
+  given(a: "person", simulation: ${JSON.stringify(simulation.id)})
+}
+`);
+          person = given;
+        });
+
+      it('creates a person', function*() {
+        expect(person.data).toMatchObject({ name: "Bob Dobalina" });
+      });
+    });
+  });
+});
