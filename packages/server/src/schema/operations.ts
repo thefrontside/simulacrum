c0f5b11a8152ef923cdabbdb0e80ca2b95db778d
Charles Lowell <cowboyd@frontside.io>
2021-04-01 12:25:10 +0300

    👍🏼rename and document createResolve() -> uncover()

--- packages/server/src/schema/operations.ts
+++ packages/server/src/schema/operations.ts
@@ -1,11 +1,17 @@
 import { Resolver } from './resolvers';
 import * as resolvers from './resolvers';
 
-export const createSimulation = createResolve(resolvers.createSimulation);
+export const createSimulation = uncover(resolvers.createSimulation);
+export const given = uncover(resolvers.given);
 
-export const given = createResolve(resolvers.given);
-
-function createResolve(resolver: Resolver<unknown, unknown>) {
+/**
+ * Nexus types are currently a mess. This lets us take a soundly
+ * typed resolver and replace all the safe types removed
+ * so that the schema can compile. Maybe it doesn't have to be
+ * this way, but that can be sorted out later. This is a compromise
+ * that lets us move forward while maintaining type safety.
+ */
+function uncover(resolver: Resolver<unknown, unknown>) {
   return {
     resolve(_: any, args: any, context: any): Promise<any> {
       return resolver.resolve(args, context);
