227535ac25b02646b3cff01234d3668bf22be465
Charles Lowell <cowboyd@frontside.io>
2021-05-18 14:46:15 -0500

    Add Auth0 package
    This creates an auth0 simulator package with a (currently empty)
    simulator. There is a start script which loads a simulation server
    that contains the ability to start auth0 simualtions.

---
+++ packages/auth0/src/start.ts
@@ -0,0 +1,14 @@
+import { main } from '@effection/node';
+import { createSimulationServer, Server } from '@simulacrum/server';
+import { auth0 } from '.';
+
+const port = process.env.PORT ? parseInt(process.env.PORT) : undefined
+
+main(function*() {
+  let server: Server = yield createSimulationServer({
+    port,
+    simulators: { auth0 }
+  });
+  console.log(`simulation server running at http://localhost:${server.address.port}`)
+  yield;
+})
