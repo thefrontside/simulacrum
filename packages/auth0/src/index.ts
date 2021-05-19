8822a89fd70a013ec521215ae1f4f56e863231b0
Charles Lowell <cowboyd@frontside.io>
2021-05-19 17:21:29 -0500

    Wrap person and create a username and password for them

--- packages/auth0/src/index.ts
+++ packages/auth0/src/index.ts
@@ -1,6 +1,4 @@
-import { Simulator } from "@simulacrum/server";
-import { Service } from '@simulacrum/server';
-import { createHttpApp } from '@simulacrum/server';
+import { Simulator, Service, createHttpApp, Person, person as createPerson } from "@simulacrum/server";
 
 const auth0Service: Service = {
   protocol: 'https',
@@ -9,5 +7,20 @@ const auth0Service: Service = {
 
 export const auth0: Simulator = () => ({
   services: { auth0: auth0Service },
-  scenarios: {}
+  scenarios: {
+    /**
+     * Here we just wrap the internal `person` scenario to augment
+     * it with a username and password
+     * but what we really need to have some way to _react_ to the person
+     * having been created and augment the record at that point.
+     */
+    *person(store, faker) {
+      let person: Person = yield createPerson(store, faker);
+      let email = faker.internet.email(person.name, undefined);
+      let password = faker.internet.password();
+      let augmented = { ...person, email, password };
+      store.slice('people').slice(person.id).set(augmented);
+      return augmented;
+    }
+  }
 });
