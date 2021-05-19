9d6fcda9da77632a07efaea844636aeda80b7c10
Paul Cowan <paul.cowan@cutting.scot>
2021-05-19 20:41:47 +0100

    add exports to server

--- packages/auth0/src/index.ts
+++ packages/auth0/src/index.ts
@@ -1,6 +1,13 @@
 import { Simulator } from "@simulacrum/server";
+import { Service } from '@simulacrum/server';
+import { createHttpApp } from '@simulacrum/server';
+
+const auth0Service: Service = {
+  protocol: 'https',
+  app: createHttpApp()
+};
 
 export const auth0: Simulator = () => ({
-  services: {},
+  services: { auth0: auth0Service },
   scenarios: {}
 });
--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,2 +1,4 @@
 export { createSimulationServer, Server } from './server';
 export { Simulator, ServerOptions } from './interfaces';
+export type { Service } from './interfaces';
+export * from './http';
