eca118f2c056ae2d783e73bf5d30de85f87cfd0c
Taras <tarasm@gmail.com>
2021-05-22 11:23:17 -0400

    Added missing types for react

--- packages/ui/app/app.tsx
+++ packages/ui/app/app.tsx
@@ -1,20 +1,23 @@
-import 'graphiql/graphiql.css';
-import { GraphiQL } from 'graphiql';
-import { createGraphiQLFetcher } from '@graphiql/toolkit';
-import React, { ReactElement } from 'react';
+import "graphiql/graphiql.css";
+import { GraphiQL } from "graphiql";
+import { createGraphiQLFetcher } from "@graphiql/toolkit";
 
 export interface AppProps {
   server: () => URL;
 }
 
-export function App({ server }: AppProps): ReactElement {
+export function App({ server }: AppProps): JSX.Element {
   let url = server();
 
-  let websocketEndpoint = new URL(url);
+  let websocketEndpoint = new URL(`${url}`);
   websocketEndpoint.protocol = "ws";
 
-  return <GraphiQL fetcher={createGraphiQLFetcher({
-    url: `${url}`,
-    subscriptionUrl: `${websocketEndpoint}`
-  })}/>;
+  return (
+    <GraphiQL
+      fetcher={createGraphiQLFetcher({
+        url: `${url}`,
+        subscriptionUrl: `${websocketEndpoint}`,
+      })}
+    />
+  );
 }
