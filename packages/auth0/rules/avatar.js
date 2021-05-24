ab100c7b2162c586e94a2276b12b86fc3c14f06f
Paul Cowan <paul.cowan@cutting.scot>
2021-05-24 15:33:33 +0100

    check user against atom store

--- packages/auth0/rules/avatar.js
+++ packages/auth0/rules/avatar.js
@@ -1,10 +1,13 @@
 /*
  simple fake rule to add an avatar
 */
+
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 function avatar(user, context, callback) {
   // eslint-disable-next-line @typescript-eslint/no-var-requires
   user.picture = require('faker').internet.avatar();
+
+  console.log(`added avatar ${user.picture} to user`);
 
   callback(null, user, context);
 }
