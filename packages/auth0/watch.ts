55a34ee5d5214e0ab2d32724155be5e59e9c0894
Paul Cowan <paul.cowan@cutting.scot>
2021-05-26 14:37:23 +0100

    update auth0 watch script

--- packages/auth0/watch.ts
+++ packages/auth0/watch.ts
@@ -1,5 +1,4 @@
-import { Operation, Stream, Task, on, sleep } from 'effection';
-import type { Process } from '@effection/node';
+import { Operation, Stream, Task, on, sleep, spawn } from 'effection';
 import { main, exec, daemon, StdIO } from '@effection/node';
 import { watch } from 'chokidar';
 
@@ -35,23 +34,21 @@ function writeOut(channel: Stream<string>, out: NodeJS.WriteStream) {
   });
 }
 
-function executeAndOut(command: string): Operation<void> {
-  return function* (task) {
-    let p: Process = exec(`npm run ${command}`).run(task);
-    task.spawn(writeOut(p.stdout, process.stdout));
-    task.spawn(writeOut(p.stderr, process.stderr));
-    yield p.expect();
-  };
+function* executeAndOut(command: string): Operation<void> {
+  let { stdout, stderr, expect } = yield exec(`npm run ${command}`);
+  yield spawn(writeOut(stdout, process.stdout));
+  yield spawn(writeOut(stderr, process.stderr));
+  yield expect();
 }
 
-function* buildAndRun(scope: Task) {
+function* buildAndRun() {
   try {
     yield executeAndOut('clean');
     yield executeAndOut('prepack');
 
-    let server: StdIO = daemon('node dist/start.js').run(scope);
-    scope.spawn(writeOut(server.stdout, process.stdout));
-    scope.spawn(writeOut(server.stderr, process.stderr));
+    let server: StdIO = yield daemon('node dist/start.js');
+    yield spawn(writeOut(server.stdout, process.stdout));
+    yield spawn(writeOut(server.stderr, process.stderr));
   } catch (err) {
     console.error(err);
   }
