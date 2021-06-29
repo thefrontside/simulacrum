7c22f799629e69859c04938f9dbedc7f775bf1a8
Paul Cowan <paul.cowan@cutting.scot>
2021-06-29 22:03:11 +0100

    flow up until /oauth/token

--- packages/auth0/src/index.ts
+++ packages/auth0/src/index.ts
@@ -3,9 +3,12 @@ import { createHttpApp } from '@simulacrum/server';
 import { urlencoded, json } from 'express';
 import { createAuth0Handlers } from './handlers/auth0-handlers';
 import { person } from '@simulacrum/server';
+import { createSession } from './middleware/session';
 import path from 'path';
 import express from 'express';
 import { Options } from './types';
+import { createCors } from './middleware/create-cors';
+import { noCache } from './middleware/no-cache';
 
 const publicDir = path.join(process.cwd(), 'src', 'views', 'public');
 
@@ -20,20 +23,28 @@ const createAuth0Service = (handlers: ReturnType<typeof createAuth0Handlers>): S
     protocol: 'https',
     app: createHttpApp()
           .use(express.static(publicDir))
+          .use(createSession())
+          .use(createCors())
+          .use(noCache())
           .use(json())
           .use(urlencoded({ extended: true }))
           .get('/heartbeat', handlers['/heartbeat'])
           .get('/authorize', handlers['/authorize'])
           .get('/login', handlers['/login'])
+          .post('/usernamepassword/login', handlers['/usernamepassword/login'])
+          .get('/u/login', handlers['/usernamepassword/login'])
+          .post('/login/callback', handlers['/login/callback'])
 
   } as const;
 };
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-export const auth0: Simulator<Options> = (authOptions) => {
-  let { options } = authOptions.get().options;
+export const auth0: Simulator<Options> = (slice, options) => {
+  let { options: { services = {} } } = slice.get();
+  let store = slice.slice('store');
 
-  let handlers = createAuth0Handlers({ ...DefaultOptions, ...options });
+  // TODO: this will only work if auth0 has been assigned a static port
+  let handlers = createAuth0Handlers({ ...DefaultOptions, ...options, store, port: services['auth0']?.port });
 
   return {
     services: { auth0: createAuth0Service(handlers) },
