46f27cd17a03c66d15f86e61071ef22039699dcc
Jordan Nieuwhof <jgnieuwhof@gmail.com>
2021-07-09 15:53:37 -0300

    add docs for why this is here

--- packages/graphql-starwars/src/start.ts
+++ packages/graphql-starwars/src/start.ts
@@ -6,6 +6,11 @@ import { schema, createContext } from '.';
 
 const port = process.env.PORT ? parseInt(process.env.PORT) : 5000;
 
+/**
+ * Serves the "real" Star Wars schema
+ *
+ * Use `@simulacrum/graphql/src/start` to run the schema in simulation
+ */
 main(function*() {
   let app = express();
   app.use('/', graphqlHTTP({
