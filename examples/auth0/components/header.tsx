2fcafb502e88bab4eedf81307128eaff996d388b
Jacob Bolda <me@jacobbolda.com>
2021-07-15 10:06:46 -0500

    slim example styles

---
+++ examples/auth0/components/header.tsx
@@ -0,0 +1,83 @@
+import React from "react";
+import Link from "next/link";
+import { useUser } from "@auth0/nextjs-auth0";
+
+const Header = (): React.ReactElement => {
+  let { user } = useUser();
+
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <Link href="/">
+              <a>Home</a>
+            </Link>
+          </li>
+          {user ? (
+            <>
+              <li>
+                <Link href="/profile">
+                  <a>Profile</a>
+                </Link>
+              </li>{" "}
+              <li>
+                <a href="/profile-ssr">Profile (SSR)</a>
+              </li>{" "}
+              <li>
+                <a href="/api/auth/logout" data-testid="logout">
+                  Logout
+                </a>
+              </li>
+            </>
+          ) : (
+            <>
+              <li>
+                <a href="/api/auth/login" data-testid="login">
+                  Login
+                </a>
+              </li>
+            </>
+          )}
+        </ul>
+      </nav>
+
+      <style jsx>{`
+        header {
+          padding: 0.2rem;
+          color: #fff;
+          background-color: #333;
+        }
+        nav {
+          max-width: 42rem;
+          margin: 1.5rem auto;
+        }
+        ul {
+          display: flex;
+          list-style: none;
+          margin-left: 0;
+          padding-left: 0;
+        }
+        li {
+          margin-right: 1rem;
+        }
+        li:nth-child(3) {
+          margin-right: auto;
+        }
+        a {
+          color: #fff;
+          text-decoration: none;
+        }
+        button {
+          font-size: 1rem;
+          color: #fff;
+          cursor: pointer;
+          border: none;
+          background: none;
+        }
+      `}</style>
+    </header>
+  );
+};
+
+export default Header;
--- examples/auth0/pages/index.tsx
+++ examples/auth0/pages/index.tsx
@@ -1,18 +1,42 @@
+import React from "react";
 import { useUser } from "@auth0/nextjs-auth0";
 
-export default function Index(): JSX.Element {
+import Layout from "../components/layout";
+
+export default function Home(): JSX.Element {
   let { user, error, isLoading } = useUser();
 
-  if (isLoading) return <div>Loading...</div>;
-  if (error) return <div>{error.message}</div>;
+  return (
+    <Layout>
+      <h1>Next.js and Auth0 Example</h1>
 
-  if (user) {
-    return (
-      <div>
-        Welcome {user.name}! <a href="/api/auth/logout">Logout</a>
-      </div>
-    );
-  }
+      {isLoading && <p>Loading login info...</p>}
 
-  return <a href="/api/auth/login">Login</a>;
+      {error && (
+        <>
+          <h4>Error</h4>
+          <pre>{error.message}</pre>
+        </>
+      )}
+
+      {user && (
+        <>
+          <h4>Rendered user info on the client</h4>
+          <pre data-testid="profile">{JSON.stringify(user, null, 2)}</pre>
+        </>
+      )}
+
+      {!isLoading && !error && !user && (
+        <>
+          <p>
+            To test the login click in <i>Login</i>
+          </p>
+          <p>
+            Once you have logged in you should be able to click in{" "}
+            <i>Protected Page</i> and <i>Logout</i>
+          </p>
+        </>
+      )}
+    </Layout>
+  );
 }
