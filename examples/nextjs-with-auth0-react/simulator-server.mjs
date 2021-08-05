0b3c666e9a99151a71ef530f98b874979f03cf73
Jacob Bolda <me@jacobbolda.com>
2021-08-05 13:00:05 -0500

    cypress tests in nextjs-with-auth0-react

--- examples/nextjs-with-auth0-react/simulator-server.mjs
+++ examples/nextjs-with-auth0-react/simulator-server.mjs
@@ -16,30 +16,27 @@ main(function* () {
 
   console.log(`simulation server running at ${url}`);
 
-  let client = createClient(url);
-
-  let simulation = yield client.createSimulation("auth0", {
-    options: {
-      audience: "https://your-audience/",
-      scope: "openid profile read:shows",
-      clientId: "YOUR_AUTH0_CLIENT_ID",
-      rulesDirectory: "./rules",
-    },
-    services: {
-      auth0: {
-        port: 4400, // port for the auth0 service itself
+  if (process.argv[2] !== "cypress") {
+    let client = createClient(url);
+    let simulation = yield client.createSimulation("auth0", {
+      options: {
+        audience: "https://your-audience/",
+        scope: "openid profile read:shows",
+        clientId: "YOUR_AUTH0_CLIENT_ID",
+        rulesDirectory: "./rules",
       },
-    },
-  });
-
-  console.log(`auth0 service running at ${simulation.services[0].url}`);
-
-  let person = yield client.given(simulation, "person");
-
-  console.log(`store populated with user`);
-  console.log(
-    `username = ${person.data.email} password = ${person.data.password}`
-  );
-
+      services: {
+        auth0: {
+          port: 4400, // port for the auth0 service itself
+        },
+      },
+    });
+    console.log(`auth0 service running at ${simulation.services[0].url}`);
+    let person = yield client.given(simulation, "person");
+    console.log(`store populated with user`);
+    console.log(
+      `username = ${person.data.email} password = ${person.data.password}`
+    );
+  }
   yield; // this keeps the server running, a function of effection
 });
