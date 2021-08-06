eae44ce0cadcb223b8e1c0cb467b0b230178595e
Paul Cowan <paul.cowan@cutting.scot>
2021-08-06 15:34:40 +0100

    abstract more simulation into cy commands

--- integrations/cypress/src/cypress.ts
+++ integrations/cypress/src/cypress.ts
@@ -1,7 +1,11 @@
 /* eslint-disable @typescript-eslint/no-namespace */
 /// <reference types="cypress" />
 
+import { Auth0ClientOptions } from '@auth0/auth0-spa-js';
+import { createClient, Simulation } from '@simulacrum/client';
 import { auth0Client } from './auth';
+
+export interface Person { email: string; password: string }
 
 interface Token {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -11,29 +15,68 @@ interface Token {
   id_token: Record<string, any>
 }
 
+
 declare global {
   namespace Cypress {
     interface Chainable {
-      login({ currentUser }: { currentUser: string }): Promise<Token>;
-      logout(): Promise<void>;
+      createSimulation(options: Auth0ClientOptions): Chainable<Simulation>;
+      login({ currentUser }: { currentUser: string }): Chainable<Token>;
+      logout(): Chainable<void>;
+      given(simulation: Simulation, attrs?: Partial<Person>): Chainable<Person>
     }
   }
 }
 
-Cypress.Commands.add('login', async ({ currentUser }: {currentUser: string}) => {
+const port = process.env.PORT || 4000;
+
+let client = createClient(`http://localhost:${port}`);
+
+Cypress.Commands.add('createSimulation', (options: Auth0ClientOptions) => {
+  Cypress.log({
+    name: 'auth0-simulator-create=simulation',
+  });
+
+  let { domain, client_id, ...auth0Options } = options;
+
+  let port = Number(domain.split(':').slice(-1)[0]);
+
+  cy.log(JSON.stringify({ domain, client_id, ...auth0Options }));
+
+  return cy.wrap(client.createSimulation("auth0", {
+    options: {
+      ...auth0Options,
+      clientId: client_id,
+    },
+    services: {
+      auth0: {
+        port
+      }
+    }
+  }));
+});
+
+Cypress.Commands.add('given', (simulation: Simulation, attrs: Partial<Person> = {}) => {
+  Cypress.log({
+    name: 'auth0-simulator-given',
+  });
+
+  return cy.wrap(client.given(simulation, "person", attrs).then(scenario => scenario.data));
+});
+
+Cypress.Commands.add('login', ({ currentUser }: { currentUser: string }) => {
   Cypress.log({
     name: 'auth0-simulator-login',
   });
 
-  await auth0Client.getTokenSilently({ ignoreCache: true, currentUser });
+  return cy.wrap(auth0Client.getTokenSilently({ ignoreCache: true, currentUser }));
 });
 
-Cypress.Commands.add('logout', async () => {
+Cypress.Commands.add('logout', () => {
   Cypress.log({
     name: 'auth0-simulator-logout',
   });
 
-  await auth0Client.logout();
+  return cy.wrap(auth0Client.logout());
 });
 
-export {};
+export { };
