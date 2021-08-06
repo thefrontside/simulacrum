4b81616c32f1db435eccb84f283b81a8d7ce814a
Paul Cowan <paul.cowan@cutting.scot>
2021-08-06 16:22:54 +0100

    make login optional child command

--- integrations/cypress/cypress/integration/login.spec.ts
+++ integrations/cypress/cypress/integration/login.spec.ts
@@ -3,12 +3,13 @@ import configJson from "../../cypress.env.json";
 describe('log in', () => {
   it('should get token without signing in', () => {
     cy.createSimulation(configJson)
-      .given()
-      .then((person) => cy.login({ currentUser: person.email }))
+      .given({ email: 'bob@gmail.com' })
+      .login()
       .then(() => {
          cy.visit('/');
 
          cy.contains('Log out');
-      }).logout();
+      })
+      .logout();
   });
 });
