ada9d95ef276b9225db5d0240d146c801bcd03a5
Jacob Bolda <me@jacobbolda.com>
2021-08-19 16:38:43 -0500

    WIP

--- integrations/cypress/cypress/integration/login.spec.ts
+++ integrations/cypress/cypress/integration/login.spec.ts
@@ -1,24 +1,23 @@
 // import { Simulation } from '@simulacrum/client';
-import { Simulation } from '../../../../packages/client/dist';
-import auth0Config from '../../cypress.example.env.json';
-import { Person } from '../../src/cypress';
+import { Simulation } from "../../../../packages/client/dist";
+import auth0Config from "../../cypress.example.env.json";
+import { Person } from "../../src/cypress";
 
-describe('auth', () => {
-  describe.skip('log in, create person per test', () => {
-    it('should get token without signing in', () => {
+describe("auth", () => {
+  describe("log in, create person per test", () => {
+    it("should get token without signing in", () => {
       cy.createSimulation(auth0Config)
         .given()
         .login()
         .then(() => {
-          cy.visit('/');
-
-          cy.contains('Log out');
+          cy.visit("/");
+          cy.contains("Log out");
         })
         .logout();
     });
   });
 
-  describe('log in, create person once', () => {
+  describe("log in, create person once", () => {
     let simulation: Cypress.Chainable<Simulation>;
     let person: Cypress.Chainable<Person>;
 
@@ -27,15 +26,48 @@ describe('auth', () => {
       person = simulation.given();
     });
 
-    it('should login', () => {
+    it("should login", () => {
       person
         .login()
         .then(() => {
-          cy.visit('/');
-
-          cy.contains('Log out');
+          cy.visit("/");
+          cy.contains("Log out");
         })
         .logout();
+    });
+  });
+
+  describe("logout in beforeEach", () => {
+    let simulation: Cypress.Chainable<Simulation>;
+    let person: Cypress.Chainable<Person>;
+
+    beforeEach(() => {
+      // log out from previous test run
+      if (person) {
+        person.logout();
+      }
+
+      simulation = cy.createSimulation(auth0Config);
+      console.dir(simulation);
+      person = simulation.given();
+    });
+
+    it("should login", () => {
+      cy.visit("/");
+      cy.contains("Log in");
+      person.login().then(() => {
+        cy.visit("/");
+        cy.contains("Log out");
+      });
+    });
+
+    it("should login again ", () => {
+      cy.visit("/");
+      cy.contains("Log in");
+      person.login().then(() => {
+        cy.visit("/");
+        cy.contains("Log out");
+      });
     });
   });
 });
