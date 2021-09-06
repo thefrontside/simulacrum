99a1eba5ca27123f1cabcb652a1cae530ff4c2c2
Paul Cowan <paul.cowan@cutting.scot>
2021-09-06 11:27:08 +0100

    wrap createSimulation in error boundary

--- packages/server/src/effect.ts
+++ packages/server/src/effect.ts
@@ -32,7 +32,6 @@ export function map<A>(slice: Slice<Record<string, A>>, effect: Effect<A>): Oper
         if (!keep.has(key)) {
           effects.delete(key);
           yield effect.halt();
-        }
       }
     }
 
