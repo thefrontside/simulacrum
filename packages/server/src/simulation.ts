f0b4ff20e3ab55d3bd653f371568f614506a5bae
Paul Cowan <paul.cowan@cutting.scot>
2021-09-06 11:27:08 +0100

    wrap createSimulation in error boundary

--- packages/server/src/simulation.ts
+++ packages/server/src/simulation.ts
@@ -10,110 +10,112 @@ import { middlewareHandlerIsOperation, isRequestHandler } from './guards/guards'
 export function simulation(simulators: Record<string, Simulator>): Effect<SimulationState> {
   return slice => function*(scope) {
     try {
-      let store = slice.slice("store");
-      let simulatorName = slice.get().simulator;
-      let simulator = simulators[simulatorName];
-      assert(simulator, `unknown simulator ${simulatorName}`);
-      let { options = {}, services: serviceOptions = {} } = slice.get().options;
+      yield function * errorBoundary() {
+        let store = slice.slice("store");
+        let simulatorName = slice.get().simulator;
+        let simulator = simulators[simulatorName];
+        assert(simulator, `unknown simulator ${simulatorName}`);
+        let { options = {}, services: serviceOptions = {} } = slice.get().options;
 
-      let behaviors = simulator(slice, options);
+        let behaviors = simulator(slice, options);
 
-      let servers = Object.entries(behaviors.services).map(([name, service]) => {
-        let app = express();
+        let servers = Object.entries(behaviors.services).map(([name, service]) => {
+          let app = express();
 
-        for(let handler of service.app.middleware) {
-          if(isRequestHandler(handler)) {
-            app.use(handler);
+          for(let handler of service.app.middleware) {
+            if(isRequestHandler(handler)) {
+              app.use(handler);
 
-            continue;
+              continue;
+            }
+
+            app.use(function(req, res, next) {
+              assert(middlewareHandlerIsOperation(handler), 'invalid middleware function');
+
+              scope.run(handler(req, res))
+              .then(next)
+              .catch(next);
+            });
           }
 
-          app.use(function(req, res, next) {
-            assert(middlewareHandlerIsOperation(handler), 'invalid middleware function');
+          app.use(raw({ type: "*/*" }));
 
-            scope.run(handler(req, res))
-            .then(next)
-            .catch(next);
-          });
+          for (let handler of service.app.handlers) {
+            app[handler.method](handler.path, (request, response) => {
+              // if the scope is already shutting down or shut down
+              // just ignore this request.
+              if (scope.state === 'running') {
+                scope.run(function*() {
+                  try {
+                    yield handler.handler(request, response);
+                  } catch(err) {
+
+                    response.status(500);
+                    response.write('server error');
+                  } finally {
+                    response.end();
+                  }
+                });
+              }
+            });
+          }
+
+          let { protocol } = service;
+
+          return {
+            name,
+            protocol,
+            create: createServer(app, { protocol, port: serviceOptions[name]?.port })
+          };
+        });
+
+        let services: {name: string; url: string; }[] = [];
+        for (let { name, protocol, create } of servers) {
+          let server: Server = yield create;
+          let address = server.address;
+          services.push({ name, url: `${protocol}://localhost:${address.port}` });
         }
 
-        app.use(raw({ type: "*/*" }));
+        let { scenarios, effects } = behaviors;
 
-        for (let handler of service.app.handlers) {
-          app[handler.method](handler.path, (request, response) => {
-            // if the scope is already shutting down or shut down
-            // just ignore this request.
-            if (scope.state === 'running') {
-              scope.run(function*() {
-                try {
-                  yield handler.handler(request, response);
-                } catch(err) {
+        // we can support passing a seed to a scenario later, but let's
+        // just hard-code it for now.
+        let faker = createFaker(2);
 
-                  response.status(500);
-                  response.write('server error');
-                } finally {
-                  response.end();
-                }
-              });
-            }
-          });
+        yield spawn(map(slice.slice("scenarios"), slice => function*() {
+          try {
+            let { name, params } = slice.get();
+            let fn = scenarios[name];
+            assert(fn, `unknown scenario ${name}`);
+
+            let data = yield fn(store, faker, params);
+            slice.update(state => ({
+              ...state,
+              status: 'running',
+              data
+            }));
+          } catch (error) {
+            slice.update(state => ({
+              ...state,
+              status: 'failed',
+              error
+            }));
+          }
+        }));
+
+        if(typeof effects !== 'undefined') {
+          yield spawn(effects());
         }
 
-        let { protocol } = service;
+        slice.update(state => ({
+          ...state,
+          status: 'running',
+          services
+        }));
 
-        return {
-          name,
-          protocol,
-          create: createServer(app, { protocol, port: serviceOptions[name]?.port })
-        };
-      });
-
-      let services: {name: string; url: string; }[] = [];
-      for (let { name, protocol, create } of servers) {
-        let server: Server = yield create;
-        let address = server.address;
-        services.push({ name, url: `${protocol}://localhost:${address.port}` });
-      }
-
-      let { scenarios, effects } = behaviors;
-
-      // we can support passing a seed to a scenario later, but let's
-      // just hard-code it for now.
-      let faker = createFaker(2);
-
-      yield spawn(map(slice.slice("scenarios"), slice => function*() {
-        try {
-          let { name, params } = slice.get();
-          let fn = scenarios[name];
-          assert(fn, `unknown scenario ${name}`);
-
-          let data = yield fn(store, faker, params);
-          slice.update(state => ({
-            ...state,
-            status: 'running',
-            data
-          }));
-        } catch (error) {
-          slice.update(state => ({
-            ...state,
-            status: 'failed',
-            error
-          }));
-        }
-      }));
-
-      if(typeof effects !== 'undefined') {
-        yield spawn(effects());
-      }
-
-      slice.update(state => ({
-        ...state,
-        status: 'running',
-        services
-      }));
-
-      // all spun up, we can just wait.
-      yield;
+        // all spun up, we can just wait.
+        yield;
+      };
     } catch (error) {
       slice.update((state) => ({
         ...state,
@@ -122,5 +124,6 @@ export function simulation(simulators: Record<string, Simulator>): Effect<Simula
         services: []
       }));
     }
+
   };
 }
