fb0a8a61699b87ea950f8dc7b9999f744c854866
Paul Cowan <paul.cowan@cutting.scot>
2021-09-06 21:09:29 +0100

    more

--- packages/server/src/http.ts
+++ packages/server/src/http.ts
@@ -82,7 +82,7 @@ export interface HttpHandler {
 }
 
 export type RouteHandler = {
-  method: 'get' | 'post' | 'put' | 'delete';
+  method: 'get' | 'post' | 'put';
   path: string;
   handler: HttpHandler;
 }
@@ -97,7 +97,6 @@ export interface HttpApp {
   get(path: string, handler: HttpHandler): HttpApp;
   put(path: string, handler: HttpHandler): HttpApp;
   post(path: string, handler: HttpHandler): HttpApp;
-  delete(path: string, handler: HttpHandler): HttpApp;
   use(middleware: Middleware | RequestHandler): HttpApp;
 }
 
@@ -116,7 +115,6 @@ export function createHttpApp(handlers: RouteHandler[] = [], middleware: (Middle
     get: (path, handler) => appendHandler({ path, handler, method: 'get' }),
     post: (path, handler) => appendHandler({ path, handler, method: 'post' }),
     put: (path, handler) => appendHandler({ path, handler, method: 'put' }),
-    delete: (path, handler) => appendHandler({ path, handler, method: 'delete' }),
     use: (middlewareHandler) => appendMiddleware(middlewareHandler),
   };
 }
