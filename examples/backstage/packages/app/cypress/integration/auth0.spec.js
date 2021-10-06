8fd434668325da0b8eab70aaf4d940e4317d29cd
wKich <w@kich.dev>
2021-10-06 16:06:03 +0200

    fix client id and certs

--- examples/backstage/packages/app/cypress/integration/auth0.spec.js
+++ examples/backstage/packages/app/cypress/integration/auth0.spec.js
@@ -2,16 +2,13 @@ import { Button } from '@interactors/with-cypress';
 import config from '../../cypress.env.json';
 
 describe('log in', () => {
-  beforeEach(() => {
-    cy.logout();
-  });
-
   it('should get token without signing in', () => {
     cy.createSimulation(config)
       .visit('/')
       .given({ email: 'fourth@gmail.com', password: 'passw0rd' })
       .login()
       .visit('/')
-      .contains('Log out');
+      .contains('Catalog')
+      .logout();
   });
 });
