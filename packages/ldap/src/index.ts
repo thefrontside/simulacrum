b6170fb16658d45861c869ff4d5980efcef26dc1
Paul Cowan <paul.cowan@cutting.scot>
2021-10-14 07:26:58 +0100

    shut down ldap server

--- packages/ldap/src/index.ts
+++ packages/ldap/src/index.ts
@@ -28,130 +28,130 @@ export function createLdapService<T extends UserData>(ldapOptions: LDAPOptions, 
     return {
       name: 'ldap service',
       *init() {
-        try {
-          let port = ldapOptions.port ?? (yield getPort());
-          let baseDN = ldapOptions.baseDN;
-          let bindDn = ldapOptions.bindDn;
-          let bindPassword = ldapOptions.bindPassword;
-          let groupDN = ldapOptions.groupDN;
-          let users = state.slice('store', 'people').get();
+        let port = ldapOptions.port ?? (yield getPort());
+        let baseDN = ldapOptions.baseDN;
+        let bindDn = ldapOptions.bindDn;
+        let bindPassword = ldapOptions.bindPassword;
+        let groupDN = ldapOptions.groupDN;
+        let users = state.slice('store', 'people').get();
 
-          assert(!!users, 'no scenarios in people/store');
+        assert(!!users, 'no scenarios in people/store');
 
-          let employees = Object.values(users).map(u => ({ ...u, id: u.email })) as T[];
+        let employees = Object.values(users).map(u => ({ ...u, id: u.email })) as T[];
 
-          let log = {
-            debug: () => undefined,
-            trace: () => undefined,
-            warn: () => undefined,
-            error: () => undefined,
-          };
+        let log = {
+          debug: () => undefined,
+          trace: () => undefined,
+          warn: () => undefined,
+          error: () => undefined,
+        };
 
-          server = createServer({ log });
-          // eslint-disable-next-line @typescript-eslint/no-explicit-any
-          server.search(baseDN, function (req: any, res: any, next: any) {
-            console.log(dedent`--- User Search ---
-            dn:     ${req.dn.toString()}
-            scope:  ${req.scope}
-            filter: ${req.filter.toString()}
-            `);
+        server = createServer({ log });
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        server.search(baseDN, function (req: any, res: any, next: any) {
+          console.log(dedent`--- User Search ---
+          dn:     ${req.dn.toString()}
+          scope:  ${req.scope}
+          filter: ${req.filter.toString()}
+          `);
 
-            for (let entry of employees) {
-              let groups = [`cn=users,${groupDN}`];
+          for (let entry of employees) {
+            let groups = [`cn=users,${groupDN}`];
 
-              let employee = {
-                dn: `cn=${entry.id},${baseDN}`,
-                attributes: {
-                  objectclass: ['user'],
-                  uid: entry.id,
-                  ...entry,
-                  memberof: groups,
-                },
-              };
+            let employee = {
+              dn: `cn=${entry.id},${baseDN}`,
+              attributes: {
+                objectclass: ['user'],
+                uid: entry.id,
+                ...entry,
+                memberof: groups,
+              },
+            };
 
-              if (req.filter.matches(employee.attributes)) {
-                console.log(`Sending ${employee.attributes.email}`);
-                res.send(employee);
-              }
+            if (req.filter.matches(employee.attributes)) {
+              console.log(`Sending ${employee.attributes.email}`);
+              res.send(employee);
             }
+          }
 
+          res.end();
+
+          return next();
+        });
+
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        server.compare(groupDN, (req: any, res: any) => {
+          console.log('--- Compare ---');
+          console.log(`DN: ${req.dn.toString()}`);
+          console.log(`attribute name: ${req.attribute}`);
+          console.log(`attribute value: ${req.value}`);
+
+          res.end(true);
+        });
+
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        server.bind(baseDN, function (req: any, res: any, next: any) {
+          console.log('--- Bind ---');
+          console.log(`bind DN: ${req.dn.toString()}`);
+          console.log(`bind PW: ${req.credentials}`);
+
+          let commonName = req.dn.rdns[0].attrs.cn.value;
+          if (!commonName) {
+            return next(new NoSuchObjectError(req.dn.toString()));
+          }
+
+          let password = req.credentials;
+          console.log('verify:', commonName, password);
+
+
+          let employee = employees.filter(u => u.id === commonName)?.[0];
+
+          if (typeof employee === 'undefined') {
+            console.log('could not find employee');
+            return next(new NoSuchObjectError(req.dn.toString()));
+          }
+
+          if (employee.password !== password) {
+            console.log(`bad password ${password} for ${employee.email}`);
+            return next(new InvalidCredentialsError(req.dn.toString()));
+          }
+
+          if (commonName === bindDn && password === bindPassword) {
+            console.log(`bind succeeded for ${bindDn}`);
             res.end();
+          } else {
+            return next(new OperationsError('could not find user'));
+          }
+        });
 
-            return next();
-          });
+        server.listen(port, function () {
+          console.log(dedent`LDAP test server running on port ${port});
 
-          // eslint-disable-next-line @typescript-eslint/no-explicit-any
-          server.compare(groupDN, (req: any, res: any) => {
-            console.log('--- Compare ---');
-            console.log(`DN: ${req.dn.toString()}`);
-            console.log(`attribute name: ${req.attribute}`);
-            console.log(`attribute value: ${req.value}`);
+          BindDN: bindDn = ${bindDn} cn=${bindDn},${baseDN}
+          Bind Password: ${bindPassword}
 
-            res.end(true);
-          });
+          UserBaseDN:    ${bindDn}
+          `);
+        });
 
-          // eslint-disable-next-line @typescript-eslint/no-explicit-any
-          server.bind(baseDN, function (req: any, res: any, next: any) {
-            console.log('--- Bind ---');
-            console.log(`bind DN: ${req.dn.toString()}`);
-            console.log(`bind PW: ${req.credentials}`);
+        yield spawn(function* shutdown() {
+          try {
+            yield;
+          } finally {
+            yield new Promise<void>(resolve => {
+              server?.unbind(() => 'unbound');
+              server?.close(() => {
+                console.log('ldap server closed');
+                resolve();
+              });
+            });
+          }
+        });
 
-            let commonName = req.dn.rdns[0].attrs.cn.value;
-            if (!commonName) {
-              return next(new NoSuchObjectError(req.dn.toString()));
-            }
-
-            let password = req.credentials;
-            console.log('verify:', commonName, password);
-
-
-            let employee = employees.filter(u => u.id === commonName)?.[0];
-
-            if (typeof employee === 'undefined') {
-              console.log('could not find employee');
-              return next(new NoSuchObjectError(req.dn.toString()));
-            }
-
-            if (employee.password !== password) {
-              console.log(`bad password ${password} for ${employee.email}`);
-              return next(new InvalidCredentialsError(req.dn.toString()));
-            }
-
-            if (commonName === bindDn && password === bindPassword) {
-              console.log(`bind succeeded for ${bindDn}`);
-              res.end();
-            } else {
-              return next(new OperationsError('could not find user'));
-            }
-          });
-
-          server.listen(port, function () {
-            console.log(dedent`LDAP test server running on port ${port});
-  
-            BindDN: bindDn = ${bindDn} cn=${bindDn},${baseDN}
-            Bind Password: ${bindPassword}
-  
-            UserBaseDN:    ${bindDn}
-            `);
-          });
-
-          yield spawn(function* shutdown() {
-            try {
-              yield;
-            } finally {
-              yield new Promise(resolve => server.unbind(resolve));
-            }
-          });
-
-          return {
-            port,
-            protocol: 'ldap'
-          };
-        } catch(e){
-          console.error(e);
-        } finally {
-          server?.close(() => console.log('ldap server closed'));
-        }
+        return {
+          port,
+          protocol: 'ldap'
+        };
       }
     };
   };
