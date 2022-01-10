8910d64a283dee16fcdc6533e95da83518ef6d51
Paul Cowan <paul.cowan@cutting.scot>
2022-01-10 20:54:33 +0000

    remove unnecessary eslint comments

--- integrations/cypress/encrypt.js
+++ integrations/cypress/encrypt.js
@@ -1,6 +1,4 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-/* eslint-disable @typescript-eslint/no-empty-function */
-/* eslint-disable @typescript-eslint/no-unused-vars */
 const hkdf = require('futoin-hkdf');
 const { JWK, JWE } = require('jose');
 
