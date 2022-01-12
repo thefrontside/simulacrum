e083f20e79249798b3c030c0c5a2d30f6054c496
Paul Cowan <paul.cowan@cutting.scot>
2022-01-12 14:41:51 +0000

    lazy load auth get-user-info

--- integrations/cypress/cypress/support/commands/authorization_code/get-user-info.ts
+++ integrations/cypress/cypress/support/commands/authorization_code/get-user-info.ts
@@ -1,13 +1,13 @@
-import { auth } from './auth';
-
 Cypress.Commands.add('getUserInfo', (accessToken) => {
   return new Cypress.Promise((resolve, reject) => {
-    auth.client.userInfo(accessToken, (err, user) => {
-      if (err) {
-        reject(err);
-      }
+    import('./auth').then(m => m.auth).then((auth) => {
+      auth.client.userInfo(accessToken, (err, user) => {
+        if (err) {
+          reject(err);
+        }
 
-      resolve(user);
+        resolve(user);
+      });
     });
   });
 });
