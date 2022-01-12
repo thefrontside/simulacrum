c3cafc0682f5929d37639d4852efed3f043c919c
Paul Cowan <paul.cowan@cutting.scot>
2022-01-12 21:12:20 +0000

    authorization flow command maker

--- integrations/cypress/cypress/support/commands/logout.ts
+++ integrations/cypress/cypress/support/commands/logout.ts
@@ -1,17 +1,11 @@
-import { Slice } from '@effection/atom';
-import { Simulation } from '@simulacrum/client';
-import { GetClientFromSpec, TestState } from '../types';
+import { CommandMaker } from '../types';
 import { makeCypressLogger } from '../utils/cypress-logger';
-import { SimulationId } from './constants';
-
-export interface MakeLogoutOptions {
-  atom: Slice<TestState>;
-  getClientFromSpec: GetClientFromSpec;
-}
 
 const log = makeCypressLogger('simulacrum-logout');
 
-export const makeLogout = ({ atom, getClientFromSpec }: MakeLogoutOptions) => () => {
-  cy.request('/api/auth/logout');
-  cy.reload();
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export const makeLogout = ({ atom, getClientFromSpec }: CommandMaker) => () => {
+  log('logging out');
+  return cy.request('/api/auth/logout')
+            .reload();
 };
