38a9e7de792cd2e4cedd072afda5f0fff4742d01
Paul Cowan <paul.cowan@cutting.scot>
2022-01-12 10:55:54 +0000

    updates to auth0-cypress

--- examples/create-react-app/cypress/plugins/index.ts
+++ examples/create-react-app/cypress/plugins/index.ts
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 
-const { encrypt } = require('@simulacrum/auth0-cypress/encrypt');
+import { encrypt } from '@simulacrum/auth0-cypress/encrypt';
 
 module.exports = (on: Cypress.PluginEvents) => {
-  on('encrypt', encrypt);
+  on('task', {encrypt});
 };
