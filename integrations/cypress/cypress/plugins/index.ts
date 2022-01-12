2b05e4e09d512ad2bca03e3fa99e77514530852f
Paul Cowan <paul.cowan@cutting.scot>
2022-01-12 23:15:55 +0000

    refactor tests

--- integrations/cypress/cypress/plugins/index.ts
+++ integrations/cypress/cypress/plugins/index.ts
@@ -1,10 +1,5 @@
 import { encrypt } from '../support/utils/encrypt';
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-function log(arg: any) {
-  console.log(arg);
-}
-
 export default (on: Cypress.PluginEvents) => {
-  on('task', { encrypt, log });
+  on('task', { encrypt });
 };
