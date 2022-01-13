dc92a1cc375737c759c476f8de1e7d2f3554a38d
Paul Cowan <paul.cowan@cutting.scot>
2022-01-13 20:25:56 +0000

    remove log task

--- integrations/cypress/cypress/support/commands/auth0_react/logout.ts
+++ integrations/cypress/cypress/support/commands/auth0_react/logout.ts
@@ -2,7 +2,6 @@ import { makeCypressLogger } from '../../utils/cypress-logger';
 
 const log = makeCypressLogger('simulacrum-logout-pkce');
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const makeLogout = () => () => {
   log('logging out');
 
