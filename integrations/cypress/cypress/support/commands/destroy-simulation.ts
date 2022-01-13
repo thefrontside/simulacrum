f1239f6c14636c459b73391ca47ae64900e8d0f9
Paul Cowan <paul.cowan@cutting.scot>
2022-01-13 15:24:57 +0000

    remove destroy simulation from logout

--- integrations/cypress/cypress/support/commands/destroy-simulation.ts
+++ integrations/cypress/cypress/support/commands/destroy-simulation.ts
@@ -5,17 +5,8 @@ import { SimulationId } from './constants';
 
 const log = makeCypressLogger('simulacrum-destroy-simulation');
 
-export const makeDestroySimulation = ({ atom, getClientFromSpec }: CommandMaker) => () => {
+export const makeDestroySimulation = ({ getClientFromSpec }: CommandMaker) => () => {
   return new Cypress.Promise((resolve, reject) => {
-    let existing = atom.slice(Cypress.spec.name).get()?.simulation;
-
-    if(typeof existing === 'undefined') {
-      log('no simulation to destroy');
-      resolve();
-    } else {
-      log(`destroying simulation ${existing.status}`);
-    }
-
     let client = getClientFromSpec(Cypress.spec.name);
 
     client.destroySimulation({ id: SimulationId } as Simulation).then(() => {
