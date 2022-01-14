71ff81fe807413c4205e75fa8f7703edb3c03c04
Paul Cowan <paul.cowan@cutting.scot>
2022-01-14 10:45:09 +0000

    call deleteSimulation on a running simulation in createSimulation

--- packages/server/src/schema/resolvers.ts
+++ packages/server/src/schema/resolvers.ts
@@ -28,6 +28,10 @@ export const createSimulation: Resolver<CreateSimulationParameters, SimulationSt
 
     let simulation = atom.slice("simulations", id);
 
+    if(simulation.get()?.status === 'running') {
+      await destroySimulation.resolve({ id }, ctx);
+    }
+
     if(['halted', 'destroying'].includes(simulation.get()?.status)) {
       await scope.run(simulation.filter((sim) => typeof sim === 'undefined').expect());
     }
