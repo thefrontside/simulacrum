117d193f78eb7d236f43684d88558a8dd8df35e9
Paul Cowan <paul.cowan@cutting.scot>
2022-11-30 13:16:08 +0000

    export named types from github-api-simulator

--- packages/github-api/src/index.ts
+++ packages/github-api/src/index.ts
@@ -1,2 +1,13 @@
+export type {
+  GithubAccount,
+  GithubCommit,
+  GithubOrganization,
+  GithubRef,
+  GithubRepository,
+  GithubTeam,
+  GithubTeamMembership,
+  User,
+  RepositoryOwner
+ } from './types/graphql';
 export * from './service/standaloneServer';
 export * from './main';
