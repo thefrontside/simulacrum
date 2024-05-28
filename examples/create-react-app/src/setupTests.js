0664c4dc9370b0deaf5bd29ea370c4785cc0ce83
Jacob Bolda <me@jacobbolda.com>
2024-05-28 09:52:40 -0500

    remove outdated examples (#275)
    * remove outdated examples
    
    * update covector config

--- examples/create-react-app/src/setupTests.js
+++
@@ -1,7 +0,0 @@
-import initFontAwesome from "./utils/initFontAwesome";
-
-// Init fonts
-initFontAwesome();
-
-// Test mocks
-require("jest-fetch-mock").enableMocks();
--- examples/create-react-app/src/views/__tests__/Home.test.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import Home from "../Home";
-
-it("renders", () => {
-  const div = document.createElement("div");
-  ReactDOM.render(<Home />, div);
-});
