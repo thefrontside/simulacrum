974671efd545c5f139aa143d863a28513865e76a
Jacob Bolda <me@jacobbolda.com>
2024-07-22 14:30:10 -0500

    add simulationRoutes to store, display at base / route

--- packages/foundation/src/store/schema.ts
+++ packages/foundation/src/store/schema.ts
@@ -8,6 +8,15 @@ export type ExtendSimulationSchemaInput<T> = ({
   slice,
 }: ExtendSimulationSchema) => T;
 
+export interface SimulationRoute {
+  type: "OpenAPI" | "Explicit";
+  url: string;
+  method: "get" | "post" | "delete" | "patch";
+  calls: number;
+  defaultCode: number;
+  responses: number[];
+}
+
 export function generateSchemaWithInputSlices<ExtendedSimulationSchema>(
   inputSchema: ExtendSimulationSchemaInput<ExtendedSimulationSchema>
 ) {
@@ -16,6 +25,16 @@ export function generateSchemaWithInputSlices<ExtendedSimulationSchema>(
   let schemaAndInitialState = createSchema({
     cache: immerSlice.table({ empty: {} }),
     loaders: immerSlice.loaders(),
+    simulationRoutes: immerSlice.table<SimulationRoute>({
+      empty: {
+        type: "Explicit",
+        url: "",
+        method: "get",
+        calls: 0,
+        defaultCode: 200,
+        responses: [200],
+      },
+    }),
     ...slices,
   });
 
