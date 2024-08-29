99d187705786528d0ca93e4c253c082048196b56
Jacob Bolda <me@jacobbolda.com>
2024-08-28 22:03:36 -0500

    rename folder to graphql

--- packages/github-api/src/index.ts
+++ packages/github-api/src/index.ts
@@ -1,6 +1,6 @@
 import { createFoundationSimulationServer } from "@simulacrum/foundation-simulator";
 import { extendStore } from "./store/index";
-import { extendRouter } from "./service/extend-api";
+import { extendRouter } from "./graphql/extend-api";
 
 export const simulation = createFoundationSimulationServer({
   port: 3300, // default port
