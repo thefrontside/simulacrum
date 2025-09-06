d0e793396e794cbf971e61ebf243f7456b81c84e
Jacob Bolda <me@jacobbolda.com>
2025-09-06 03:03:37 -0500

    start handling app install access tokens

--- packages/github-api/src/rest/index.ts
+++ packages/github-api/src/rest/index.ts
@@ -27,6 +27,31 @@ const handlers =
         }));
         response.status(200).json(data);
       },
+      // POST /app/installations/{installation_id}/access_tokens
+      "apps/create-installation-access-token": async (
+        _context,
+        _request,
+        _response
+      ) => {
+        const repositories =
+          simulationStore.selectors.allReposWithOrgs(
+            simulationStore.store.getState()
+          ) ?? [];
+        const token = "ghs_16C7e42F292c6912E7710c838347Ae178B4a";
+        return {
+          status: 201,
+          json: {
+            token,
+            expires_at: "2030-07-11T22:14:10Z",
+            permissions: {
+              issues: "write",
+              contents: "read",
+            },
+            repository_selection: "selected",
+            repositories,
+          },
+        };
+      },
       // L#4134 /installation/repositories
       "apps/list-repos-accessible-to-installation": async (
         _context,
